a93afc05d2f092c58d2e12d47f14c2a8118e1b50
Bayjeolog35 <155692394+Bayjeolog35@users.noreply.github.com>
2025-07-03 20:46:43 +0300

    Update generate_country_html.py

--- generate_country_html.py
+++ generate_country_html.py
@@ -431,8 +431,9 @@ def generate_html_file(country_folder_name, videos_data, structured_data):
         is_current_country_active = " active" if c_folder_name == country_folder_name else ""
         
         # Class attribute'ine hem genel 'country-button' sınıfını hem de 'active' sınıfını ekliyoruz.
-    country_buttons_html.append(
-    f'''<button onclick="location.href='../{c_folder_name}/'" data-letter="{first_letter}" class="country-button{is_current_country_active}">{c_display_name}</button>'''
+    sanitized_folder_name = c_folder_name.replace("_", "-")
+country_buttons_html.append(
+    f'''<button onclick="location.href='../{sanitized_folder_name}/'" data-letter="{first_letter}" class="country-button{is_current_country_active}">{c_display_name}</button>'''
 )
 
 
