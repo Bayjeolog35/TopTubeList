f1926902fc1d81db9fbd9a1bf5b18e4684058ef2
Bayjeolog35 <155692394+Bayjeolog35@users.noreply.github.com>
2025-07-06 23:18:15 +0300

    Update generate_all_html.py

--- generate_all_html.py
+++ generate_all_html.py
@@ -378,14 +378,11 @@ def build_html(name, videos, structured_data, country_panel_html):
     readable_name = name.replace("-", " ").title()
 
       # 🔹 Structured Data (JSON-LD Script Bloğu)
-    structured_data_block = ""
-    if structured_data:
-        structured_json = json.dumps(structured_data, indent=2, ensure_ascii=False)
-        structured_data_block = f"""
-        <script type="application/ld+json">
+    structured_data_block = f"""
+<script type="application/ld+json">
 {structured_json}
-        </script>
-        """
+</script>
+"""
 
 
     # 🔹 iframe: İlk videodan embedUrl ve title çek
