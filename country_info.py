2542e60a10694708aacc2e7b9a8b19690f71295e
Bayjeolog35 <155692394+Bayjeolog35@users.noreply.github.com>
2025-07-10 00:46:31 +0300

    Update country_info.py

--- country_info.py
+++ country_info.py
@@ -1,5 +1,5 @@
 COUNTRY_INFO = {
-    "afghanistan": {"code": "AF", "continent": "asia"},
+    "afghanistan": {"code": "af", "continent": "asia"},
     "albania": {"code": "al", "continent": "europe"},
     "algeria": {"code": "dz", "continent": "africa"},
     "andorra": {"code": "ad", "continent": "europe"},
